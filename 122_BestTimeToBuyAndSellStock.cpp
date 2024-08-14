1de077a6a701547bef9485729e01be05ba182f93
Önder Hamamcıoğlu <onder1hamamcioglu@gmail.com>
2024-08-14 13:33:40 +0300

    122 minor change

--- 122_BestTimeToBuyAndSellStock.cpp
+++ 122_BestTimeToBuyAndSellStock.cpp
@@ -13,7 +13,7 @@ public:
         int profit = 0;
         for(int i = 0; i < prices.size()-1; i++)
         {
-            if(prices[i+1]>prices[i])
+            if(prices[i+1] > prices[i])
                 profit = profit + prices[i+1] - prices[i];
         }
         return profit;
